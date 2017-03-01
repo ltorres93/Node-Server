3e34d15a343daab19b3cbb101bdfd72561e9d891
ltorres93 <cheak93@gmail.com>
2017-03-01 15:47:48 +0100

    Update server.js
    Incluido el boarding pass

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require ('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var router = express.Router();
+
+app.use(router);
+router.all('/', function (req, res, next) {  
+  console.log('Someone made a request!');
+  next();
+});
 
 
 var checkin = [];
@@ -13,7 +20,7 @@ checkin[0]= ({
         "template_type": "airline_checkin",
         "intro_message": "Welcome Mr $surname",
         "locale": "en_US",
-		"theme_color":"#d61a2b",
+    "theme_color":"#d61a2b",
         "pnr_number": "Code",
         "flight_info": [
           {
@@ -43,14 +50,97 @@ checkin[0]= ({
   }]
 })
 
-app.get ('/*', function(req, res){
-	var surname = req.param ('surname');
-	var codigo = req.param ('codigo');
-	checkin[0].messages[0].attachment.payload.intro_message= (`Checkin is available Mr ${surname}`);
-	checkin[0].messages[0].attachment.payload.pnr_number= (`${codigo}`);
-	 res.setHeader('Content-type', 'application/json');
-	res.json((checkin[0]));
+var BoardingPass = [];
+BoardingPass[0]= (
+{
+ "messages": [
+  {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "airline_boardingpass",
+        "intro_message": "You are checked in.",
+        "locale": "en_US",
+        "theme_color":"#d61a2b",
+        "boarding_pass": 
+          {
+            "passenger_name": "LUIS TORRES",
+            "pnr_number": "CG4X7U",
+            "travel_class": "business",
+            "seat": "74J",
+            "auxiliary_fields": [
+              {
+                "label": "Terminal",
+                "value": "T1"
+              },
+              {
+                "label": "Departure",
+                "value": "30OCT 19:05"
+              }
+            ],
+            "secondary_fields": [
+              {
+                "label": "Boarding",
+                "value": "18:30"
+              },
+              {
+                "label": "Gate",
+                "value": "D57"
+              },
+              {
+                "label": "Seat",
+                "value": "74J"
+              },
+              {
+                "label": "Sec.Nr.",
+                "value": "003"
+              }
+            ],
+            "logo_image_url": "http://www.outono.net/elentir/wp-content/uploads/2013/10/10334967323_f1b359c78a_o.png",
+            "header_image_url": "https:\/\/www.example.com\/en\/fb\/header.png",
+            "qr_code": "M1SMITH\/NICOLAS  CG4X7U nawouehgawgnapwi3jfa0wfh",
+            "above_bar_code_image_url": "https:\/\/www.example.com\/en\/PLAT.png",
+            "flight_info": {
+              "flight_number": "KL0642",
+              "departure_airport": {
+                "airport_code": "JFK",
+                "city": "New York",
+                "terminal": "T1",
+                "gate": "D57"
+              },
+              "arrival_airport": {
+                "airport_code": "AMS",
+                "city": "Amsterdam"
+              },
+              "flight_schedule": {
+                "departure_time": "2016-01-02T19:05",
+                "arrival_time": "2016-01-05T17:30"
+              }
+            }
+          }
+      }
+    }
+  }]
+})
+
+router.get ('/', function(req, res){
+  global.surname = req.param ('surname');
+  global.codigo = req.param ('codigo');
+  checkin[0].messages[0].attachment.payload.intro_message= (`Checkin is available Mr ${surname}`);
+  checkin[0].messages[0].attachment.payload.pnr_number= (`${codigo}`);
+  res.setHeader('Content-type', 'application/json');
+  res.json((checkin[0]));
 });
 
+
+
+router.get ('/bpass', function(req, res){
+  global.surname = req.param ('surname');
+  global.codigo = req.param ('codigo');
+  BoardingPass[0].messages[0].attachment.payload.boarding_pass.passenger_name= (`Mr ${surname}`);
+  BoardingPass[0].messages[0].attachment.payload.boarding_pass.pnr_number= (`${codigo}`);
+  res.setHeader('Content-type', 'application/json');
+  res.json((BoardingPass[0]));
+});
 app.listen (port);
 console.log (`Server started at ${port}`)
