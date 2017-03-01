99ad2f1d47aadaa886623def69a77d763b37567c
ltorres93 <cheak93@gmail.com>
2017-03-01 16:05:47 +0100

    Update server.js
    Another try

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ checkin[0]= ({
         "template_type": "airline_checkin",
         "intro_message": "Welcome Mr $surname",
         "locale": "en_US",
-    "theme_color":"#d61a2b",
+        "theme_color":"#d61a2b",
         "pnr_number": "Code",
         "flight_info": [
           {
@@ -62,8 +62,8 @@ BoardingPass[0]= (
         "intro_message": "You are checked in.",
         "locale": "en_US",
         "theme_color":"#d61a2b",
-        "boarding_pass": [
-          {
+        "boarding_pass": 
+          [{
             "passenger_name": "LUIS TORRES",
             "pnr_number": "CG4X7U",
             "travel_class": "business",
@@ -95,7 +95,7 @@ BoardingPass[0]= (
                 "label": "Sec.Nr.",
                 "value": "003"
               }
-            ],]
+            ],
             "logo_image_url": "http://www.outono.net/elentir/wp-content/uploads/2013/10/10334967323_f1b359c78a_o.png",
             "header_image_url": "https:\/\/www.example.com\/en\/fb\/header.png",
             "qr_code": "M1SMITH\/NICOLAS  CG4X7U nawouehgawgnapwi3jfa0wfh",
@@ -117,7 +117,7 @@ BoardingPass[0]= (
                 "arrival_time": "2016-01-05T17:30"
               }
             }
-          }
+          }]
       }
     }
   }]
